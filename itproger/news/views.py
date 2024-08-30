c0a6881753aa5206bbe5c22b95d4f97225aad1cd
Dmitriy_Klimenko <klimati1000@gmail.com>
2024-08-30 19:34:24 +0300

    first

---
+++ itproger/news/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+from .models import Articles
+from .forms import ArticlesForm
+
+
+# Create your views here.
+def news_home(request):
+    news = Articles.objects.all()
+    return render(request, 'news/news_home.html', {'news': news})
+
+def create(request):
+    form = ArticlesForm()
+
+    data = {
+        'form': form
+    }
+
+    return render(request, 'news/create.html', data)
